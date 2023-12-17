65ef8f60e4734c027badd6392345588754e9527c
Acoidán Rodríguez Peña <acoidanrp13@gmail.com>
2023-12-17 17:46:55 +0000

    Interfaz Histogram acabada

--- src/main/java/software/ulpgc/kata3/HistogramCalculator.java
+++
@@ -1,7 +0,0 @@
-package software.ulpgc.kata3;
-
-import java.util.Map;
-
-public interface HistogramCalculator {
-    Map<String,Integer> calculate();
-}
--- src/main/java/software/ulpgc/kata3/WeightHistogramCalculator.java
+++ src/main/java/software/ulpgc/kata3/WeightHistogramCalculator.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class WeightHistogramCalculator implements HistogramCalculator{
+public class WeightHistogramCalculator implements Histogram {
     private static final int BIN_SIZE = 20;
     private final List<Person> people;
 
