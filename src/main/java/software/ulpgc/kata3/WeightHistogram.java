be0b64e9db7d3f8de5529d9be32e6fb8b383dce2
Acoidán Rodríguez Peña <acoidanrp13@gmail.com>
2023-12-17 17:48:38 +0000

    Clase WeightHistogram acabada

---
+++ src/main/java/software/ulpgc/kata3/WeightHistogram.java
@@ -0,0 +1,27 @@
+package software.ulpgc.kata3;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WeightHistogram implements Histogram {
+    private final List<Person> people;
+
+    public WeightHistogram(List<Person> people) {
+        this.people = people;
+    }
+
+    @Override
+    public int bins() {
+        return 6;
+    }
+
+    @Override
+    public double[] values() {
+        double[] values = new double[people.size()];
+        for (int i = 0; i < people.size() ; i++) {
+            values[i] = people.get(i).getWeight();
+        }
+        return values;
+    }
+}
